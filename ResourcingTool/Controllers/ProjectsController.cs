9e7bcd1d7c294a0de0f11dce677096e097743f85
Daniel Callaghan <callaghan@outlook.com>
2018-08-01 16:58:21 +0100

    fixed create method.

--- ResourcingTool/Controllers/ProjectsController.cs
+++ ResourcingTool/Controllers/ProjectsController.cs
@@ -49,7 +49,7 @@ namespace ResourcingTool.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        
         public ActionResult Create([Bind(Include = "ProjectId,ProjectName,ProjectSector,ProjectClient,ProjectLead,ProjectNature,ProjectScope,ContractStart,ContractFinish,Briefing,Fieldwork,Analysis,Reporting,Debrief,KeyInformation,SpecialRequirements,NumDirectorsRequired,NumSeniorManagersRequired,NumManagersRequired,NumSeniorAssociate2Required,NumSeniorAssociate1Required,NumAssociate2Required,NumAssociate1Required,DaysDirectorsRequired,DaysSeniorManagersRequired,DaysManagersRequired,DaysSeniorAssociate2Required,DaysSeniorAssociate1Required,DaysAssociate2Required,DaysAssociate1Required,ResponseNeededBy,fk_UserId_Requester")] Project project)
         {
             if (ModelState.IsValid)
