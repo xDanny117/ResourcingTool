e3d2e8e8dc0bd6691762502806a2911091ff1942
Stephan <stephan.x.williams@pwc.com>
2018-08-06 16:58:53 +0100

    Some edit visual changes, no functionality

--- ResourcingTool/Content/table/approveDeny.js
+++ ResourcingTool/Content/table/approveDeny.js
@@ -22,3 +22,17 @@ $(".denyRow").click(function () {
 
     $('#denyForm').submit();
 });
+
+
+$(".editRow").click(function () {
+
+    var thisID = $(this).parent().parent().attr('id');
+    var str = "" + thisID;
+
+    console.log(str);
+
+    $("#denyID").val(thisID);
+
+
+    $('#denyForm').submit();
+});
