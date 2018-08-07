9939af259c2d9ec0f0b75035f64da5b6ed79d29e
Stephan <stephan.x.williams@pwc.com>
2018-08-07 13:38:48 +0100

    Solving the commit error

--- ResourcingTool/Content/table/approveDeny.js
+++ ResourcingTool/Content/table/approveDeny.js
@@ -22,3 +22,48 @@ $(".denyRow").click(function () {
 
     $('#denyForm').submit();
 });
+
+$(".approveRow2").click(function () {
+
+    var thisID = $(this).parent().parent().parent().parent().attr('id');
+
+    console.log(thisID);
+
+    var str = thisID.substring(5, thisID.length);
+
+    console.log(str);
+
+    $("#approveID").val(str);
+
+    $('#approveForm').submit();
+});
+
+$(".denyRow2").click(function () {
+
+    var thisID = $(this).parent().parent().parent().parent().attr('id');
+
+    console.log(thisID);
+
+    var str = thisID.substring(5, thisID.length);
+
+    console.log(str);
+
+    $("#denyID").val(str);
+
+
+    $('#denyForm').submit();
+});
+
+
+$(".editRow").click(function () {
+
+    var thisID = $(this).parent().parent().attr('id');
+    var str = "" + thisID;
+
+    console.log(str);
+
+    $("#denyID").val(thisID);
+
+
+    $('#denyForm').submit();
+});
--- ResourcingTool/Models/User.cs
+++ ResourcingTool/Models/User.cs
@@ -19,5 +19,6 @@ namespace ResourcingTool.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+        public string Email { get; set; }
     }
 }
