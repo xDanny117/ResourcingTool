ba397d82dbdf3e689b25c2974ad764b64e01d789
Daniel Callaghan <callaghan@outlook.com>
2018-10-12 16:31:13 +0100

    remove special reqs mandatory and making sector mandatory

--- ResourcingTool/Models/ResourcingTool.Designer.cs
+++ ResourcingTool/Models/ResourcingTool.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\929364\source\repos\ResourcingTool\ResourcingTool\Models\ResourcingTool.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\929366\source\repos\ResourcingTool\ResourcingTool\Models\ResourcingTool.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
